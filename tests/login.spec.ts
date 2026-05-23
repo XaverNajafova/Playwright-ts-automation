fc31f10ac7695daedcace7e94ef7179d66b4e308
XaverNajafova <nurixaver@gmail.com>
2026-05-23 22:54:38 +0400

    Initial Playwright framework with login tests

---
+++ tests/login.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test('valid login test', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await page.goto('https://www.saucedemo.com/');
+
+  await loginPage.login('standard_user', 'secret_sauce');
+
+  await expect(page).toHaveURL(/inventory/);
+});
